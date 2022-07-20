fd1df58e7a7b3971a02dad6b55dc7dac29bc8f16
Bharti Kashyap <bhartikashyap@Bhartis-MacBook-Pro.local>
2022-07-20 20:50:18 +0530

    done

--- newAamirprojectBackend/server.js
+++ newAamirprojectBackend/server.js
@@ -16,5 +16,5 @@ app.use('/user', require('./users/users.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8080;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8000;
 app.listen(port, () => console.log('Server listening on port ' + port));
